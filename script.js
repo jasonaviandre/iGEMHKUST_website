1a5d79d56ef7f0754fadf10d144d7fff35fe6606
jasonaviandre <66403561+jasonaviandre@users.noreply.github.com>
2020-07-15 19:46:16 +0700

    Update

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const nav = document.querySelector(".nav-btn");
-const body = document.querySelector("header");
-
-const options = {
-    rootMargin: "-40px 0px 0px 0px"
-};
-
-const headerObserver = new IntersectionObserver(function(entries, headerObserver) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            nav.classList.add("whiteBg");
-            console.log("test");
-        } else {
-            nav.classList.remove("whiteBg");
-        }
-    });
-}, options);
-
-headerObserver.observe(body);
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".container");
+    header.classList.toggle("whiteBg", window.scrollY > 0);
+})
