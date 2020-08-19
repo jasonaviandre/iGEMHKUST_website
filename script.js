dc5f652e1c0c2dfb45c1b87eac0591af043e9de0
jasonaviandre <66403561+jasonaviandre@users.noreply.github.com>
2020-08-19 18:04:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.addEventListener("scroll", function() {
-    var header = document.querySelector(".container");
+    var header = document.querySelector(".container1");
+    var logo = document.querySelector(".logo img");
     header.classList.toggle("whiteBg", window.scrollY > 0);
+    logo.classList.toggle("logoSmall", window.scrollY > 0);
 })
